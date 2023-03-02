4b958909fd99d8f719a6ff3335f22d3314fa29d6
jfgen <jfgencarnacao@gmail.com>
2023-03-02 18:33:23 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import { useState } from "react";
+import "./App.css";
+import {
+  CreateNote,
+  NavBar,
+  NoteUICollection,
+  UpdateNote,
+} from "./ui-components";
+
+function App() {
+  const [showCreateModal, setShowCreateModal] = useState(false);
+  const [showUpdateModal, setShowUpdateModal] = useState(false);
+  const [updateNote, setUpdateNote] = useState();
+
+  return (
+    <>
+      <NavBar
+        width="100%"
+        marginBottom="20px"
+        overrides={{
+          Button31632483: { onClick: () => setShowCreateModal(true) },
+        }}
+      />
+      <div className="container">
+        <NoteUICollection
+          overrideItems={({ item, index }) => {
+            return {
+              overrides: {
+                EditButton: {
+                  onClick: () => {
+                    setShowUpdateModal(true);
+                    setUpdateNote(item);
+                  },
+                },
+              },
+            };
+          }}
+        />
+      </div>
+      <div
+        className="modal"
+        style={{ display: showCreateModal === false && "none" }}
+      >
+        <CreateNote
+          overrides={{
+            MyIcon: {
+              as: "button",
+              onClick: () => setShowCreateModal(false),
+            },
+          }}
+        />
+      </div>
+      <div
+        className="modal"
+        style={{ display: showUpdateModal === false && "none" }}
+      >
+        <UpdateNote
+          note={updateNote}
+          overrides={{
+            MyIcon: {
+              as: "button",
+              onClick: () => setShowUpdateModal(false),
+            },
+          }}
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
